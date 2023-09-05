e74c261ee4f40c06c3d1c942bce864a3ffcee797
Rasu002 <143691486+Rasu002@users.noreply.github.com>
2023-09-05 15:57:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(){
+    x=parseInt(document.getElementById("val_1").value);
+    y=parseInt(document.getElementById("val_2").value);
+
+    document.getElementById("numbers").innerHTML=x+y;
+}
+
+function sub(){
+    x=document.getElementById("val_1").value;
+    y=document.getElementById("val_2").value;
+
+    document.getElementById("numbers").innerHTML=x-y;
+}
+
+function mul(){
+    x=document.getElementById("val_1").value;
+    y=document.getElementById("val_2").value;
+
+    document.getElementById("numbers").innerHTML=x*y;
+}
+
+function div(){
+    x=document.getElementById("val_1").value;
+    y=document.getElementById("val_2").value;
+
+    document.getElementById("numbers").innerHTML=x/y;
+}
